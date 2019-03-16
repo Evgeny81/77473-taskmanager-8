78e8e4d75860417fd799272d207eb5594325e9f2
Evgeny_Kuznetsov <evgeny_kuznetsov@epam.com>
2019-03-16 15:25:50 +0300

    Module 4: remade, add editTask component

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
-import {Task} from './components/Task';
+import {Task} from './components/task';
+import {EditTask} from './components/editTask';
 import {filters as filtersConfig, card as cardConfig} from './config';
-import {filterTemplate} from './templates';
+import filterTemplate from './templates/filter';
 import {getAllElements, getElement, getRandomInt, renderElement} from './utils';
 
 const mainFilter = getElement(document, `.main__filter`);
-const boardTasks = getElement(document, `.board__tasks`);
+const tasksContainer = getElement(document, `.board__tasks`);
 
 const clearBoard = () => {
-  boardTasks.innerHTML = ``;
+  tasksContainer.innerHTML = ``;
   for (let i = 0; i < getRandomInt(3, 7); i++) {
     const firstTask = new Task(cardConfig());
-    boardTasks.appendChild(firstTask.render());
+    tasksContainer.appendChild(firstTask.render());
   }
 };
 
@@ -21,8 +22,19 @@ Array.from(filterElement).forEach((element) => {
   element.addEventListener(`click`, clearBoard);
 });
 
-for (let i = 0; i < 3; i++) {
-  const firstTask = new Task(cardConfig());
-  boardTasks.appendChild(firstTask.render());
-}
+const task = new Task(cardConfig());
+const editTask = new EditTask(cardConfig());
+tasksContainer.appendChild(task.render());
+
+task.onEdit = () => {
+  editTask.render();
+  tasksContainer.replaceChild(editTask.element, task.element);
+  task.unrender();
+};
+
+editTask.onSubmit = () => {
+  task.render();
+  tasksContainer.replaceChild(task.element, editTask.element);
+  editTask.unrender();
+};
 
---
+++ src/templates/task.js
@@ -0,0 +1,64 @@
+import hashtagTemplate from './hashtag';
+
+export default ({title = ``, isRepeating, color = `black`, dueDate, tags, picture}) => (
+  `<article class="card card--${color} ${isRepeating ? `card--repeat` : ``} ${dueDate < Date.now() ? `card--deadline` : ``}">
+            <form class="card__form" method="get">
+              <div class="card__inner">
+                <div class="card__control">
+                  <button type="button" class="card__btn card__btn--edit">
+                    edit
+                  </button>
+                  <button type="button" class="card__btn card__btn--archive">
+                    archive
+                  </button>
+                  <button
+                    type="button"
+                    class="card__btn card__btn--favorites card__btn--disabled"
+                  >
+                    favorites
+                  </button>
+                </div>
+
+                <div class="card__color-bar">
+                  <svg class="card__color-bar-wave" width="100%" height="10">
+                    <use xlink:href="#wave"></use>
+                  </svg>
+                </div>
+
+                <div class="card__textarea-wrap">
+                  <label>
+                    <textarea
+                      class="card__text"
+                      placeholder="Start typing your text here..."
+                      name="text"
+                    >${title}</textarea>
+                  </label>
+                </div>
+
+                <div class="card__settings">
+                  <div class="card__details">
+                    <div class="card__hashtag">
+                      <div class="card__hashtag-list">
+                        ${tags.map((tag) => hashtagTemplate(tag)).join(``)}                        
+                      </div>
+                    </div>
+                  </div>
+
+                  <label class="card__img-wrap ${picture ? `` : `card__img-wrap--empty`}">
+                    <input
+                      type="file"
+                      class="card__img-input visually-hidden"
+                      name="img"
+                    />
+                    <img
+                      src=${picture ? picture : `img/add-photo.svg`}
+                      alt="task picture"
+                      class="card__img"
+                    />
+                  </label>
+                </div>
+
+              </div>
+            </form>
+          </article>`
+);
